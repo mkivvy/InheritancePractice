60bee0e2405c2cc960fc5c50c341418742a16322
mking@my.wctc.edu <>
2013-01-24 14:41:21 -0600

    concrete superclass - 1st code

--- src/concrete/Employee.java
+++ src/concrete/Employee.java
@@ -5,5 +5,32 @@ package concrete;
  * @author Mary
  */
 public class Employee {
+    private String firstName;
+    private String lastName;
+    private String jobTitle;
+    private char payFrequency;
+
+    public Employee(String firstName, String lastName, String jobTitle, char payFrequency, double totalPeriodPay) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.jobTitle = jobTitle;
+        this.payFrequency = payFrequency;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public String getJobTitle() {
+        return jobTitle;
+    }
+
+    public char getPayFrequency() {
+        return payFrequency;
+    }
 
 }
